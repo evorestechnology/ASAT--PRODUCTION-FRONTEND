3e45da5b2eb3793e09c045aaf0a0dbb45d329336
tejanvk43 <23ng1a0543@usharama.in>
2026-08-09 12:48:46 +0530

    added the change password

--- src/pages/master/MasterSettings.jsx
+++ src/pages/master/MasterSettings.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import '../../styles/admin.css';
 import BackButton from '../../components/BackButton';
 import { apiFetch, uploadFile } from '../../api';
@@ -371,6 +372,7 @@ const inlineStyles = `
 `;
 
 function MasterSettings() {
+    const navigate = useNavigate();
     const [terms, setTerms] = useState('');
     const [shippingNote, setShippingNote] = useState('');
     const [admins, setAdmins] = useState([]);
